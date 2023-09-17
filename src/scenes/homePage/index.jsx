361c01399c88e30a9e6927e7ccc1ae35abfc7232
jonamonti <jonathanmonti@hotmail.com>
2023-09-17 10:05:59 -0300

    feat: navbar in home page

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { Box } from '@mui/material'
 import Navbar from 'scenes/navbar'
 
 const HomePage = () => {
   return (
-    <>
+    <Box>
       <Navbar />
-    </>
+    </Box>
   )
 }
 
