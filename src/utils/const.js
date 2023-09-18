cdc5a04106de876a2980a9054001ef3b33f7624f
jonamonti <jonathanmonti@hotmail.com>
2023-09-18 04:20:17 -0300

    feat: constants

---
+++ src/utils/const.js
@@ -0,0 +1,42 @@
+export const trxCategories = {
+  expense: [
+    'bills',
+    'car',
+    'clothes',
+    'eating out',
+    'entertainment',
+    'food',
+    'gifts',
+    'health',
+    'house',
+    'pets',
+    'sports',
+    'taxi',
+    'transport',
+    'travel'
+  ],
+  income: [
+    'salary',
+    'other incomes'
+  ]
+}
+// export const trxCategories = [
+//   'bills',
+//   'car',
+//   'clothes',
+//   'eating out',
+//   'entertainment',
+//   'food',
+//   'gifts',
+//   'health',
+//   'house',
+//   'pets',
+//   'sports',
+//   'taxi',
+//   'transport',
+//   'travel',
+//   'salary',
+//   'other incomes'
+// ]
+
+export const trxTypes = ['income', 'expense'];
