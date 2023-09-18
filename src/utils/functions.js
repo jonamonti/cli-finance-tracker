1ae55d802847a8ba9128ee98d602fbeafb7d421c
jonamonti <jonathanmonti@hotmail.com>
2023-09-18 04:20:32 -0300

    feat: apiAuth function

---
+++ src/utils/functions.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const ApiAuth = (token) => {
+  return (
+    axios.create({
+      baseURL: 'http://localhost:1337/',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    })    
+  )
+}
