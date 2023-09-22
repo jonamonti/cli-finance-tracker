8fd64458c38578fbfef432a6edf9ae5066210944
jonamonti <jonathanmonti@hotmail.com>
2023-09-22 17:24:29 -0300

    feat: adding toast and improving styles

--- src/components/NewTransaction.jsx
+++ src/components/NewTransaction.jsx
@@ -24,6 +24,7 @@ import { ApiAuth } from "utils/functions";
 import dayjs from 'dayjs';
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { toast } from "react-hot-toast";
 
 const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount, cDdateStr, openDialog }) => {
   const dispatch = useDispatch();
@@ -64,15 +65,14 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
     setDate(value)
   }
 
-  // const closeDialog = () => {
-  //   openDialog(false)
-  // }
 
   const handleAddTrx = async () => {
+    if (!type || !category || !amount || !description) return toast.error('Please complete all fileds!')
     const Api = ApiAuth(token);
     const response = await Api.post('transaction', { userID: _id, amount, type, category, description, date });
     const transactions = response.data;
     dispatch(setTransactions(transactions));
+    toast.success('Transaction added!')
     setAmount('');
     setDescription('');
     if (update) openDialog();
@@ -80,10 +80,12 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
   };
 
   const handleUpdateTrx = async () => {
+    if (!type || !category || !amount || !description) return toast.error('Please complete all fileds!')
     const Api = ApiAuth(token);
     const response = await Api.put('transaction', { _id: c_id, userID: _id, amount, type, category, description, date });
     const transactions = response.data;
     dispatch(setTransactions(transactions));
+    toast.success('Transaction edited!')
     setAmount('');
     setDescription('');
   };
@@ -91,7 +93,9 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
   return (
     <Box mb='2rem'  >
       <Wrapper>
-        <Box sx={{ height: '300px' }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', height: '300px', gap: '1rem' }}>
+
+          {/* TITLE */}
           <FlexBetween>
             <Typography
               variant='h4'
@@ -102,33 +106,12 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
             >
               {update ? 'Is something wrong? Edit here!' : 'New expense? Add it!'}
             </Typography>
-            {/* <Box mb={'1rem'}>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  label="Select a date"
-                  fontSize='small'
-                  value={date}
-                  maxDate={dayjs()}
-                  onChange={handleDate}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </LocalizationProvider>
-            </Box> */}
           </FlexBetween>
-          <FlexBetween gap='1rem'>
-            {/* amount */}
-            <TextField
-              fullWidth
-              placeholder="0"
-              sx={{ marginBottom: '1rem' }}
-              label="How much?"
-              value={amount}
-              onChange={handleAmount}
-              type="number"
-              inputProps={{
-                step: '0.01', // Set the step to allow decimal numbers with 2 decimal places
-              }}
-            />
+
+          {/* FIRST ROW */}
+          <Box display='flex' justifyContent='flex-start' gap='1rem'>
+
+            {/* DATE */}
             <Box mb={'1rem'}>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DatePicker
@@ -141,42 +124,52 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
                 />
               </LocalizationProvider>
             </Box>
-            {/* <TextField
-              fullWidth
-              sx={{ marginBottom: '1rem' }}
-              label="Add a description"
-              value={description}
-              onChange={handleDescription}
-              type="text"
-              inputProps={{
 
+            {/* AMOUNT */}
+            <TextField
+              // fullWidth
+              placeholder="0"
+              sx={{ marginBottom: '1rem' }}
+              label="How much?"
+              value={amount}
+              onChange={handleAmount}
+              type="number"
+              inputProps={{
+                step: '0.01', // Set the step to allow decimal numbers with 2 decimal places
               }}
-            /> */}
-          </FlexBetween>
-          {/* <FlexBetween gap='2rem'> */}
+            />
+          </Box>
+
+          {/* SECOND ROW */}
           <Box
+            // width={'100%'}
             display='flex'
+            justifyContent={isNotMobile ? 'flex-start' : 'space-between'}
             alignItems='center'
             gap='1rem'
             mb='1.5rem'
-            flexDirection={isNotMobile ? 'row' : 'column'}
+          // flexDirection={isNotMobile ? 'row' : 'column'}
           >
-            {/* trx type */}
+
+            {/* TRX TYPE */}
             <FormControl
               variant='standard'
               value={type}
               sx={{
+                display: "flex",
+                width: '50%',
                 '& .MuiInputBase-root': {
                   borderRadius: '6px',
-                  width: `${isNotMobile ? '200px' : '100%'}`
+                  // width: `${isNotMobile ? '200px' : '100%'}`
                 }
               }}>
               <Select
+                fullWidth
                 value={type}
                 onChange={handleType}
                 sx={{
                   backgroundColor: neutralLight,
-                  width: '150px',
+                  // width: '150px',
                   p: '0.25rem 1rem',
                   '& .MuiSvgIcon-root': {
                     pr: '0.25rem',
@@ -200,14 +193,16 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
               </Select>
             </FormControl>
 
-            {/* trx category */}
+            {/* TRX CATEGORY */}
             <FormControl
               variant='standard'
               value={category}
               sx={{
+                display: "flex",
+                width: '50%',
                 '& .MuiInputBase-root': {
                   borderRadius: '6px',
-                  width: '200px'
+                  // width: `${isNotMobile ? '200px' : '100%'}`
                 }
               }}>
               <Select
@@ -215,18 +210,18 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
                 onChange={handleCategory}
                 sx={{
                   backgroundColor: neutralLight,
-                  width: '200px',
+                  // width: '200px',
                   p: '0.25rem 1rem',
                   '& .MuiSvgIcon-root': {
                     pr: '0.25rem',
                     width: '3rem'
                   },
                   '& MuiSelect-select:focus': {
-                    backgroundColor: neutralLight,
+                    // backgroundColor: neutralLight,
 
                   }
                 }}
-                input={<InputBase />}
+                input={<InputBase sx={{ padding: '0 1rem' }} />}
               >
                 {trxCategories[type].map((el, i) => {
                   return (
@@ -238,35 +233,40 @@ const NewTransaction = ({ update, c_id, cType, cCategory, cDescription, cAmount,
 
               </Select>
             </FormControl>
-
           </Box>
-          {/* add trx button */}
-          <TextField
-            fullWidth
-            sx={{ marginBottom: '1rem' }}
-            label="Add a description"
-            value={description}
-            onChange={handleDescription}
-            type="text"
-            inputProps={{
 
-            }}
-          />
-          <Button
-            variant="standard"
-            type='button'
-            disabled={!date || !amount || !description}
-            onClick={update ? handleUpdateTrx : handleAddTrx}
-            sx={{
-              // m: '2rem 0',
-              // p: '1rem',
-              backgroundColor: palette.primary.main,
-              color: palette.background.alt,
-              '&:hover': { color: palette.primary.main }
-            }}>
-            {update ? 'Edit' : 'Add'}
-          </Button>
+          {/* THIRD ROW */}
+          <Box display='flex' justifyContent='flex-start' gap='1rem'>
+
+            {/* DESCRIPTION */}
+            <TextField
+              sx={{ marginBottom: '1rem', flexBasis: '70%' }}
+              label="Add a description"
+              value={description}
+              onChange={handleDescription}
+              type="text"
+              inputProps={{
+
+              }}
+            />
+            {/* ADD TRX BUTTON */}
+            <Button
+              variant="standard"
+              type='button'
+              // disabled={!date || !amount || !description}
+              onClick={update ? handleUpdateTrx : handleAddTrx}
+              sx={{
+                marginBottom: '1rem',
+                flexBasis: '30%',
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                '&:hover': { color: palette.primary.main }
+              }}>
+              {update ? 'Edit' : 'Add'}
+            </Button>
+          </Box>
         </Box>
+
 
       </Wrapper>
     </Box>
